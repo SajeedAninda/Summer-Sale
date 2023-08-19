1fa2f1df4f6661e711121cdbcdcc88f2ac686b5b
SajeedAninda <sajaninda@gmail.com>
2023-08-19 22:27:22 +0600

    Retreived and set the item prices

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+let totalPrice = parseFloat(document.querySelector("#total-price-field").innerText);
 function handleDivClicks(card) {
     // FINDING CARD ELEMENTS 
     let cardItemName = card.childNodes[5].innerText;
-    let cardItemValue = parseFloat(card.childNodes[7].innerText.split(" ")[0]);
     let itemListDiv = document.querySelector("#Item-List-Div");
     // ADDING ELEMENTS NAME TO THE CART LIST 
     const count = itemListDiv.childElementCount;
@@ -9,5 +9,11 @@ function handleDivClicks(card) {
     p.classList.add('my-4');
     p.innerHTML = `<span class="text-[#111] text-lg font-medium">${count + 1}</span>. <span class="text-[#111] text-lg font-medium">${cardItemName}</span>`;
     itemListDiv.appendChild(p);
-
+    // RETREIVING ITEM PRICES 
+    let cardItemValue = parseFloat(card.childNodes[7].innerText.split(" ")[0]);
+    let total = parseFloat(document.querySelector("#total-field").innerText);
+    // SETTING ITEM PRICE 
+    totalPrice = totalPrice + cardItemValue;
+    document.querySelector("#total-price-field").innerText = totalPrice.toFixed(2);
+    document.querySelector("#total-field").innerText = totalPrice.toFixed(2);
 }
